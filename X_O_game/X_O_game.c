3e2f9c76aa46addf9f28c5bff0535d064f20fe97
Mateusz <ganski213@gmail.com>
2024-03-30 02:03:30 +0100

    slight variable naming improvements, added exception handling mechanics

--- X_O_game/X_O_game.c
+++ X_O_game/X_O_game.c
@@ -1,58 +1,71 @@
 #include <stdio.h>
-void PopulateTable(int rows, int columns, char Table[rows][columns]) {
-    for(int i = 0; i <= rows; i++) {
-        for(int j = 0; j <= columns; j++) {
-            Table[i][j] = 'O';
+#include <ctype.h> 
+#include <stdlib.h>
+#include <string.h>
+void PopulateTable(char table[9][9]) {
+    for(int i = 0; i <= 9; i++) {
+        for(int j = 0; j <= 9; j++) {
+            table[i][j] = 'O';
         }
     }
 }
-void PrintTable(int rows, int columns, char Table[rows][columns]) {
+void PrintTable(char table[9][9]) {
     printf(" |1|2|3|4|5|6|7|8|9|\n");
-    for (int i = 0; i < rows; i++) {
-        printf("%d|%c|%c|%c|%c|%c|%c|%c|%c|%c|\n", i+1,Table[i][0],Table[i][1],Table[i][2],Table[i][3],Table[i][4],Table[i][5],Table[i][6],Table[i][7],Table[i][8]);
+    for (int i = 0; i < 9; i++) {
+        printf("%d|%c|%c|%c|%c|%c|%c|%c|%c|%c|\n", i+1,table[i][0],table[i][1],table[i][2],table[i][3],table[i][4],table[i][5],table[i][6],table[i][7],table[i][8]);
     }
 }
-int CheckIfVictory(int *counter, int rows, int columns, char Table[rows][columns]) {
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < columns; j++) {
-            if(Table[i][j] == 'X'){
+int CheckIfVictory(int *counter, char table[9][9]) {
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            if(table[i][j] == 'X'){
                 (*counter)++;
             }
         }
     }
     return *counter;
 }
-void ReplaceValue(int i, int j, char Table[9][9]){
-    switch (Table[i][j]) {
+void ReplaceValue(int i, int j, char table[9][9]){
+    switch (table[i][j]) {
         case 'X':
-            Table[i][j] = 'O';
+            table[i][j] = 'O';
             break;
         case 'O':
-            Table[i][j] = 'X';
+            table[i][j] = 'X';
             break;
     }
 }
+void takeUserMove(int* chosenRow, int* chosenColumn, int turn) {
+    char chosenMove[2];
+    printf("Insert coordinates of the cell on the table where you would like to place 'X' (in a form of double digit number, where first digit is an number of row and second number is a number of column): ");
+    scanf("%s", chosenMove);
+    if(isdigit(chosenMove[0])==0 || isdigit(chosenMove[1])==0 || strlen(chosenMove) > 2) {
+        printf("error");
+        exit(-1);
+    } else {
+        *chosenRow = (chosenMove[0]-'0');
+        *chosenColumn = (chosenMove[1]-'0');
+    }
+}
 int main() {
-    char Table[9][9];
+    char table[9][9];
     int turn = 1;
     int counter = 0;
-    int ChosenRow;
-    int ChosenColumn;
-    PopulateTable(9, 9, Table);
+    int chosenRow;
+    int chosenColumn;
+    PopulateTable(table);
+    PrintTable(table);
     while(counter < 81) {
         counter = 0;
-        printf("Turn nr.%d\nEnter the number of row, in which you want to put 'X': ", turn);
-        scanf("%d", &ChosenRow);
-        printf("Enter the number of column, in which you want to put 'X': ");
-        scanf("%d", &ChosenColumn);
-        ReplaceValue(ChosenRow-1, ChosenColumn-1, Table);
-        ReplaceValue(ChosenRow-2, ChosenColumn-1, Table);
-        ReplaceValue(ChosenRow-1, ChosenColumn-2, Table);
-        ReplaceValue(ChosenRow, ChosenColumn-1, Table);
-        ReplaceValue(ChosenRow-1, ChosenColumn, Table);
-        PrintTable(9, 9, Table);
+        takeUserMove(&chosenRow, &chosenColumn, turn);
+        ReplaceValue(chosenRow-1, chosenColumn-1, table);
+        ReplaceValue(chosenRow-2, chosenColumn-1, table);
+        ReplaceValue(chosenRow-1, chosenColumn-2, table);
+        ReplaceValue(chosenRow, chosenColumn-1, table);
+        ReplaceValue(chosenRow-1, chosenColumn, table);
+        PrintTable(table);
         turn++;
-        CheckIfVictory(&counter, 9, 9, Table);
+        CheckIfVictory(&counter,table);
     }
     printf("Victory\nAmount of turns: %d", turn);
     return 0;
